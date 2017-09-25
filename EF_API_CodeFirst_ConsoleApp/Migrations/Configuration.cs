a102401dcbfd71c1a3fb813a617b91e1e3d57ce6
onudeep <anudeepg@nrifintech.com>
2017-09-25 15:00:06 +0530

    test EF Code First approach

---
+++ EF_API_CodeFirst_ConsoleApp/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace EF_API_CodeFirst_ConsoleApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EF_API_CodeFirst_ConsoleApp.OfficeContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "EF_API_CodeFirst_ConsoleApp.OfficeContext";
+        }
+
+        protected override void Seed(EF_API_CodeFirst_ConsoleApp.OfficeContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ EF_API_CodeFirst_ConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_API_CodeFirst_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new OfficeContext())
+            {
+                Employee emp = new Employee()
+                {
+                    EmployeeName = "Chris",
+                    DateOfBirth = DateTime.Parse("jan-15-1993"),
+                    EmployeeID = 0
+                };
+
+                context.Employees.Add(emp);
+                context.SaveChanges();
+                Console.Write("Changes saved");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public Employee()
+        {
+
+        }
+
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+
+    public class OfficeContext : DbContext
+    {
+        public OfficeContext() : base()
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
